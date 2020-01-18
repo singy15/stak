7ae9c962d6986a560b71e1a898fbbec4b9687d31
kedama <kedama@mail.com>
2020-01-18 09:25:14 +0900

    Fix view definition

--- src/db/views/create_v_task.sql
+++ src/db/views/create_v_task.sql
@@ -1,3 +1,30 @@
+DROP VIEW IF EXISTS v_task;
 
-CREATE ALGORITHM=UNDEFINED SQL SECURITY DEFINER VIEW `v_task` AS select `solution`.`name` AS `solution`,`typ_task`.`name` AS `type`,`task`.`task_cd` AS `code`,`task`.`name` AS `name`,`typ_status`.`name` AS `status`,`creator`.`name` AS `created_by`,`modifier`.`name` AS `updated_by` from (((((`t_task` `task` left join `t_solution` `solution` on((`task`.`solution_cd` = `solution`.`solution_cd`))) left join `c_type` `typ_task` on(((`task`.`task_type` = `typ_task`.`type`) and (`typ_task`.`type_category` = 'TT')))) left join `c_type` `typ_status` on(((`task`.`status_type` = `typ_status`.`type`) and (`typ_status`.`type_category` = 'TS')))) left join `m_user` `creator` on((`task`.`created_by` = `creator`.`user_cd`))) left join `m_user` `modifier` on((`task`.`updated_by` = `modifier`.`user_cd`)));
+CREATE VIEW v_task AS 
+SELECT 
+  solution.name AS solution,
+  typ_task.name AS type,
+  task.task_cd AS code,
+  task.name AS name,
+  typ_status.name AS status,
+  creator.name AS created_by,
+  modifier.name AS updated_by 
+FROM t_task task 
+LEFT JOIN t_solution solution 
+ON 
+  task.solution_cd = solution.solution_cd
+LEFT JOIN c_type typ_task 
+ON 
+  task.task_type = typ_task.type_cd
+  AND typ_task.type_category = 'TT'
+LEFT JOIN c_type typ_status 
+ON 
+  task.status_type = typ_status.type_cd
+  AND typ_status.type_category = 'TS' 
+LEFT JOIN m_user creator 
+ON 
+  task.created_by = creator.user_cd
+LEFT JOIN m_user modifier 
+ON 
+  task.updated_by = modifier.user_cd
 
